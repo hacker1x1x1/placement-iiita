9df5212b8c2952bddaf6bc0d9c52f2cde6c01c69
Rudransh Gupta <34509429+hacker1x1x1@users.noreply.github.com>
2019-03-04 22:21:36 +0530

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,53 @@
+<?php
+	include("header.php");
+?>
+		<div class="row display-pic">
+			<img src="images/iiita/campus2.jpg" class="col s12 dp">
+		</div>
+		
+		<div class="row chip-bar-container hide-on-small-only">
+			<div class="col s12 m10 offset-m1 chips-bar">
+				<div class="chip">
+					<a href="#history">History</a>
+				</div>
+				<div class="chip">
+					<a href="#mission">Mission</a>
+				</div>
+			</div>
+		</div>
+
+		<div class="row block" id="history">
+			<div class="block-title col s12 valign-wrapper">
+				<span class="valign leftline"></span>
+				<p class="valign title">History</p>
+				<span class="valign rightline"></span>
+			</div>
+			<div class="col s10 offset-s1 body">
+				<p>Established in 1999 as an Institute of National Importance to be a centre of excellence in Information Technology, Electronics &amp; Communications and allied areas, Indian Institute of Information Technology, Allahabad (IIIT Allahabad) was conferred the Deemed University status by Govt. of India in the year 2000.</p>
+				<p>IIIT Allahabad now stands as an institute of international repute known for being a pioneer in the field of Information Technology. With its highly experienced faculty and one of the brightest groups of students of this nation, coupled with dynamic academic courses, inter-departmental electives, semester projects and world class research labs, the institute has succeeded in creating an environment where creativity and new ideas flourish.</p>
+				<p>The institute is committed to producing students who apart from pure academics excel in other fundamentally important area like extra-curricular activities and have exposure to the cutting edge technologies that are shaping our world. </p>
+				<!--<p>The institute also has an extension campus RGIIT Amethi to improve the quality of life of the surrounding area and to bridge the technology gap.</p>-->
+			</div>
+		</div>
+
+		<div class="row block" id="mission">
+			<div class="block-title col s12 valign-wrapper">
+				<span class="valign leftline"></span>
+				<p class="valign title">Mission</p>
+				<span class="valign rightline"></span>
+			</div>
+			<div class="col s10 offset-s1 body">
+				<p>The mission of IIIT, Allahabad is to be a unique and world class nucleating "Apex Center of Excellence" in the area of Information Technology, Electronics &amp; Communications and Allied Sciences, so as to enhance India's Technological strength in IT, Silicon and Communications industries, and produce professionals who with their tremendous competencies and sound knowledge of the leading technologies are able to contribute to the development of our nation, to establish India as the leader in Technology on the global map.</p>
+			</div>
+		</div>
+
+		<?php
+			include 'footer.php';
+		?>
+   		
+		<!--Import jQuery before materialize.js-->
+		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script type="text/javascript" src="js/materialize.min.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
+	</body>
+</html>
---
+++ academic_exchange_programs.php
@@ -0,0 +1,88 @@
+<?php
+	include("header.php");
+?>
+		<div class="row page-title">
+		 	<h2 class="white-text center-align">Academic Exchange Programs</h2> 
+		</div>
+
+		<div class="row chip-bar-container hide-on-small-only">
+			<div class="col s12 m10 offset-m1 chips-bar">
+				<div class="chip">
+					<a href="#exchange_program_institutions">Exchange programs</a>
+				</div>
+				<div class="chip">
+					<a href="#centers_of_excellence">Centers of Excellence</a>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="row block" id="exchange_program_institutions">
+			<div class="col s10 offset-s1 body">
+				<img class="responsive-img" src="images/iiita/exchangeprograms.png" />
+				<p>The Institute has continued to maintain and enhance the academic excellence it has had since its inception that has enabled it to have collaborative academic exchanges with the following International / National Universities / Institutions of academic eminence. MoUs have already been signed with most of these institutions and with the rest they are in process.</p>
+				<br>
+				<ul class="col s12 m6 l6">
+					<li>Carnegie Mellon University, Pittsburgh</li>
+					<li>California University, Riverside, USA</li>
+					<li>State University of New York, Buffalo</li>
+					<li>Massachusetts Institute of Technology, USA</li>
+					<li>Gwangju Institute of Science &amp; Technology (GIST), Korea</li>
+					<li>Canberra University, Australia</li>
+					<li>EPFL Louisiana and ETZ Zurich, Switzerland</li>
+					<li>Aalborg University, Denmark</li>
+					<li>Russian New University (ROSNOU), Moscow, Russia</li>
+					<li>University of Michigan, USA</li>
+					<li>Caledonian College of Engineering, Muscat, Oman</li>
+					<li>IIT‐Kanpur, IIT‐Mumbai and IIT‐New Delhi</li>
+					<li>Information Security Research Consortium jointly signed by USA, China, Japan, Russia, Germany, Israel, India, ROSNOU‐Russia</li>
+					<li>University of Dundee, Nethergate, Scotland, UK</li>
+					<li>Moscow Institute of Physics and Technology (State University), Moscow, Russia</li>
+					<li>Center for Teleinfrastructure (CTIF), Aalborg University, Denmark</li>
+					<li>Asian Institute of Technology, Bangkok, Thailand</li>
+					<li>The Southern Taiwan University, Taiwan</li>
+					<li>The University of Lincoln, U.K.</li>
+					<li>Erasmus MC: University Medical Centre, Rotterdam, Netherland</li>
+					<li>The Erasmus University, Rotterdam, Netherland</li>
+					<li>University of Abertay Dundee, Scotland</li>
+					<li>The Queensland University of Technology, Brisbane, Australia</li>
+					<li>PuteraSampoerna Foundation (PSF), Jakarta</li>
+					<li>M.H Alsya Co. W.L.L., Kuwait</li>
+					<li>Ohio State University &amp; Cornell University</li>
+					<li>Biolink Institute, Link Campus University, Rome, Italy</li>
+					<li>Shenyang University, China</li>
+					<li>The Swiss Federal Institute of Technology, Lausanne, Switzerland</li>
+					<li>Allahabad High Court, Allahabad, India</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row block" id="centers_of_excellence">
+			<div class="col s10 offset-s1 body">
+				<h5>Centers of Excellence</h5>
+				<ul class="col s12 m6 l6">
+					<li>Indo-Russian Centre of Biotechnology</li>
+					<li>Indo-US Centre of Language Technology</li>
+					<li>Indo – Danish Centre for Wireless Communication &amp; Sensors</li>
+					<li>Indo-Swiss Centre for Microelectronics</li>
+					<li>Patent Referral Centre</li>
+					<li>Plagiarism Detection Centre</li>
+					<li>S &amp; T Discovery Park for Rural Empowerment, Amethi</li>
+					<li>Centre for Physically Disabled Persons</li>
+					<li>I4CT, Denmark (Being established)</li>
+				</ul>
+			</div>
+
+		</div>
+
+		<?php
+			include 'footer.php';
+		?>
+
+		<!--Import jQuery before materialize.js-->
+		<!-- <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script> -->
+		<script type="text/javascript" src="js/jquery-1.12.2.js"></script>
+		<script type="text/javascript" src="js/materialize.min.js"></script>
+		<script type="text/javascript" src="js/script.js"></script>
+	</body>
+</html>
